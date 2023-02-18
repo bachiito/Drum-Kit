247db786c7f6a9679ea0a2755d48e19cf69d862d
bachiito <riveraabraham332@gmail.com>
2023-02-18 12:06:10 -0400

    fixed button permanent focus on mobile devices and playing when button is clicked

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 window.addEventListener("keydown", playSound);
+const keys = document.querySelectorAll(".key");
+
+keys.forEach(key => {
+  key.addEventListener("click", soundClicked);
+  key.addEventListener("transitionend", removePlayingTransition);
+});
 
 function playSound(e) {
-  const audio = document.querySelector(`audio[data-key-code="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key-code="${e.keyCode}"]`);
-  if (!audio) return;
+  play(e);
+}
 
+function soundClicked(e) {
+  play(e);
+}
+
+function play(e) {
+  //  We look for keyCode in a keydown event if it is a nullish
+  //  value then we look for it in the dataset of the target of a click event
+  const keyCode = e.keyCode ?? e.target.dataset.keyCode;
+  const audio = document.querySelector(`audio[data-key-code="${keyCode}"]`);
+  const key = document.querySelector(`.key[data-key-code="${keyCode}"]`);
+
+  if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
-
-  const keys = document.querySelectorAll(".key");
-  keys.forEach(key => {
-    key.addEventListener("transitionend", removePlayingTransition);
-  });
 }
 
-function removePlayingTransition(e) {
-  if (e.propertyName === "scale") this.classList.remove("playing");
+function removePlayingTransition() {
+  this.classList.remove("playing");
 }
